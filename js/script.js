ae1c1d2b1f1936f38b1993e51ba9d62528fc33b0
jdh1999 <jackhettinger@comcast.net>
2020-02-23 12:32:29 -0500

    update commit
    js / css / index

--- js/script.js
+++ js/script.js
@@ -1,27 +1,39 @@
-//Start up function to be called when the page loads
-
-//startup will establish event handlers
-
-function startup() {
-    var ev = document.getElementById("column");
+// start up function to be called when the page loads
+// startup will establish event handlers
+function startup() { //windowload
+    var btn = document.getElementById("mybtn");
+    var statusbox = document.getElementById("statusbox")
+    var startpos = 0;
+    var distance = 0;
     //start of touch
-    ev.addEventListener("touchstart", handleStart, false);
-    //end of touch
-    ev.addEventListener("touchend", handleEnd, false);
-    //cancelled touch
-    ev.addEventListener("touchcancel", handleCancel,false);
+    btn.addEventListener("touchstart", function(start) {
+        var touchArea = start.changedTouches[0]; // takes first point of touch
+        startpos = parseInt(touchArea.clientX); // takes value (positon of touch relative to the left most side of the browser) and parses to an int value.
+        statusbox.innerHTML = "Status HTML: touchstart <br> Client X: " + startpos + "px";
+        alert(this.id + "congrats u tocuhed it");
+        start.preventDefault(); // prevent default associated act from occuring
+    }, false);
+
+    // end of touch
+    btn.addEventListener("touchend", function(end) {
+        var touchArea = end.changedTouches[0];
+        statusbox.innerHTML = "Status HTML: ending touch<br> Client X: " + touchArea.clientX + "px"; 
+        end.preventDefault();
+    }, false);
+
+    // cancelled touch
+    btn.addEventListener("touchcancel", function(cancel) {
+        var touchArea;
+
+    }, false);
+
+    // movement
+    btn.addEventListener("touchmove", function(move) {
+        var touchArea = move.changedTouches[0];
+        distance = parseInt(touchArea.clientX) - startpos;
+        statusbox.innerHTML = "Status: touchmove<br> side distance traveled: " + dist + "px"
+    }, false);
 
 }
-//activate
 
-document.addEventListener("ContentLoaded", startup);
-//array to hold touches in progress
-var onGoingTouch = [];
-
-//config handleStat
-
-function handleStart(event) {
-    event.preventDefault(); // prevents browser from responding
-    console.log("im seeing a touchStart");
-
-}
+document.addEventListener("DOMContentLoaded", startup)
